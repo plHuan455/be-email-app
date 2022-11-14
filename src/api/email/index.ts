4b8c4e2a3ebd1c833c52a52d8d21ef9613da4564
Lộc Đặng <kendidang04082k@gmail.com>
2022-11-14 16:40:47 +0700

    config src base

--- src/api/email/index.ts
+++ src/api/email/index.ts
@@ -1,8 +1,26 @@
+import { EMAIL_API_URL } from './../../constants/EmailAPI/index';
 import { Receiver } from '@layouts/InformationBar';
-import { ApiResponse } from '@api/ApiClient';
+import ApiClient, { ApiResponse } from '@api/ApiClient';
+import { AxiosResponse } from 'axios';
 
 // export interface Receiver {}
 
+interface FileType {
+  email_id: number;
+  path: string;
+}
+
+export interface CreateEmailParam {
+  title: string;
+  subject: string;
+  writer_id: number;
+  to: string[];
+  from: string;
+  content: string;
+  cc: string[];
+  bcc: string[];
+  file: FileType[];
+}
 export interface EmailResponse {
   id: number;
   writer_id: number;
@@ -10,12 +28,27 @@ export interface EmailResponse {
   from: string;
 }
 
-export const getEmail = async (): Promise<ApiResponse<EmailResponse>> => {
-  const test = {
-    id: 2,
-    writer_id: 2,
-    receiver: ['asdasd'],
-    from: 'asdasd',
-  };
-  return test;
+export const getAllEmail = async (): Promise<AxiosResponse<EmailResponse>> => {
+  const url = EMAIL_API_URL;
+  const res = await ApiClient.get(url, undefined, {});
+  console.log('🚀 ~ file: index.ts ~ line 24 ~ getAllEmail ~ res', res);
+  return res;
+};
+
+export const postEmail = async (
+  param: CreateEmailParam,
+): Promise<AxiosResponse<EmailResponse>> => {
+  const url = EMAIL_API_URL;
+  const res = await ApiClient.post(url, undefined, param);
+  console.log('🚀 ~ file: index.ts ~ line 24 ~ getAllEmail ~ res', res);
+  return res;
+};
+
+export const deleteEmail = async (
+  id: string,
+): Promise<AxiosResponse<EmailResponse>> => {
+  const url = EMAIL_API_URL;
+  const res = await ApiClient.delete(`${url}/${id}`, undefined);
+  console.log('🚀 ~ file: index.ts ~ line 24 ~ getAllEmail ~ res', res);
+  return res;
 };
