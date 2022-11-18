2aaee61c5237fbede55287bcee7cc410ea1af00a
Giang Do <giangz0009@gmail.com>
2022-11-18 14:04:02 +0700

    fix

--- src/components/organisms/EmailCompose/index.tsx
+++ src/components/organisms/EmailCompose/index.tsx
@@ -158,6 +158,7 @@ function EmailCompose() {
     const checkData = await check();
 
     if (checkData) {
+      console.log(getAll());
       return toast.success('Ok!');
     }
     return toast.error('*Vui lòng nhập người nhận!');
